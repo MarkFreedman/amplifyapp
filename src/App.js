c965890c10a3bee4b684a7f3571525b7e4cb6069
Mark Freedman <mark@markfreedman.com>
2022-03-18 09:16:12 -0400

    Trying to fix auth.

--- src/App.js
+++ src/App.js
@@ -4,22 +4,13 @@ import './App.css';
 import { withAuthenticator } from '@aws-amplify/ui-react'
 
 function App() {
-  const components = {
-    Header,
-    SignIn: {
-      Header: SignInHeader,
-      Footer: SignInFooter
-    },
-    Footer
-  };
-  
   return (
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
         <h1>We now have Auth!</h1>
       </header>
-      <Authenticator components={components}>
+      <Authenticator>
           {({ signOut, user }) => (
             <main>
               <h1>Hello {user.username}</h1>
